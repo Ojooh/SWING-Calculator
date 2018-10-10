28a37de50245920ca640d291050d4a3283ca0c69
Ojooh <davidmatthew708@gmail.com>
2018-10-10 14:48:26 +0100

    JAVA GUI Calculator

---
+++ src/swingcalculator/Swingcalculator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swingcalculator;
+
+/**
+ *
+ * @author david
+ */
+public class Swingcalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        SwingarithmeticJframe sjf = new SwingarithmeticJframe();
+        sjf.setSize(400, 400);
+        sjf.setVisible(true);
+    }
+    
+}
